37ac38ecd640b0f9cfc241326fe5aa3c677cf37f
Eric Holdridge <31286414+ericholdridge@users.noreply.github.com>
2018-05-24 17:08:01 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Eric on 8/29/17.
+ */
+
+function main() {
+    // Home heading
+    $('.hero-title').hide();
+    $('.hero-title').slideDown(1000);
+}
+
+$(document).ready(main);
