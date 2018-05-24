4b8f8c74f5e5dbda276bde3841b2594bd26c2753
Eric Holdridge <31286414+ericholdridge@users.noreply.github.com>
2018-05-24 17:11:16 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Holdridge Designs</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="js/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="index.css" />
+</head>
+<body>
+
+<!-- Navigation -->
+<nav class="navbar">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">HOLDRIDGE DESIGNS</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#home">HOME</a></li>
+                <li><a href="#about">ABOUT</a></li>
+                <li><a href="#skill">SKILL</a></li>
+                <li><a href="#portfolio">PORTFOLIO</a></li>
+                <li><a href="#contact">CONTACT</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<!-- Home -->
+<section id="home">
+    <div class="container-fluid">
+        <div class="hero-holder">---
+            <div class="row-flex">
+                <div class="hero-content">
+                    <h3 class="hero-title">
+                        WEB DEVELOPER / DESIGNER
+                    </h3>
+                    <button class="btn btn-default hero-btn" id="learnBtn">START A PROJECT</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- About me -->
+<section id="about" class="about-me">
+    <div class="container">
+        <div class="about-section">
+            <div class="row">
+                <div class="about-heading">
+                    <div class="col-lg-12">
+                        <h1>About me</h1>
+                    </div>
+                </div>
+                <div class="about-sentences">
+                    <div class="row">
+                        <div class="about-s">
+                            <div class="col-lg-12">
+                                <p>Hello, my name is Eric Holdridge. I am a full stack web developer and designer
+                                located in Dubuque, IA. Computers have been in my life ever since I was growing up. My passion for web design started
+                                a few years back and have loved it ever since.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="about-icons">
+                    <div class="col-sm-4 col-md-4 col-lg-4">
+                        <div class="icons">
+                            <i class="fa fa-user" aria-hidden="true"></i>
+                        </div>
+                        <div class="icon-heading">
+                            <h1>Passion</h1>
+                            <p>Design and functionality are two of my main goals when
+                            designing websites. </p>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 col-md-4 col-lg-4">
+                        <div class="icons">
+                            <i class="fa fa-code" aria-hidden="true"></i>
+                        </div>
+                        <div class="icon-heading">
+                            <h1>Craft</h1>
+                            <p>Design and functionality are two of my main goals when
+                                designing websites. </p>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 col-md-4 col-lg-4">
+                        <div class="icons">
+                            <i class="fa fa-handshake-o" aria-hidden="true"></i>
+                        </div>
+                        <div class="icon-heading">
+                            <h1>Character</h1>
+                            <p>Design and functionality are two of my main goals when
+                                designing websites.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Skill -->
+<section id="skill" class="my-skills">
+    <div class="container">
+        <div class="skill-section">
+            <div class="row">
+                <div class="skill-heading">
+                    <div class="col-lg-12">
+                        <h1>My skill</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="strength-section">
+                    <div class="col-lg-6">
+                        <div class="skill-quote">
+                            <p>"Most designers and developers these days program not because they get
+                            paid, but because it is fun to do."</p>
+                        </div>
+                        <div class="why-programming">
+                            <p>One of the main differences in a good designer / web developer and bad, is the
+                            type of quality they put into the project. You can search for a designer
+                            on the internet and find someone instantly, but when you find a designer that
+                            loves what they do, that's when your project takes off and looks stunning. I am
+                            currently a student who loves designing projects. I charge a fairly decent price in
+                            return for experience.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="skill-progress">
+                <p>HTML / CSS</p>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">100%
+                        <span class="sr-only">100% Complete</span>
+                    </div>
+                </div>
+                <p>Javascript / jQuery </p>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">80%
+                        <span class="sr-only">20% Complete</span>
+                    </div>
+                </div>
+                <p>PHP</p>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%">70%
+                        <span class="sr-only">70% Complete</span>
+                    </div>
+                </div>
+                <p>Photoshop</p>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">80%
+                        <span class="sr-only">80% Complete</span>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- Portfolio -->
+<section id="portfolio" class="my-portfolio">
+    <div class="container">
+        <div class="portfolio-section">
+            <div class="row">
+                <div class="portfolio-heading">
+                    <div class="col-lg-12">
+                        <h1>My portfolio</h1>
+                    </div>
+                    <div class="about-s" id="gray">
+                        <div class="col-lg-12">
+                            <p>As I complete projects through freelancing, I will begin to update my portfolio as soon as possible. I have created a few websites
+                            in my general location and I am open to any freelancing design projects. If you have an idea, please feel free to contact me. I will get back to you within
+                            24 hours.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="button">
+                <a href="#contact">Contact me</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Contact -->
+<section id="contact" class="contact-me">
+    <div class="container">
+        <div class="contact-section">
+            <div class="row">
+                <div class="contact-heading">
+                    <div class="col-lg-12">
+                        <h1>Start a project!</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="send-form">
+                    <div class="col-md-8 col-md-offset-2">
+                        <form action="SendGrid/SendGrid-API/send-email.php" method="post" class="form-horizontal">
+                            <div class="form-group">
+                                <label for="exampleInputName2">Name</label>
+                                <input type="text" class="form-control" name="name" placeholder="John Doe">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputEmail2">Email</label>
+                                <input type="email" class="form-control" name="email" placeholder="example@example.com">
+                            </div>
+                            <div class="form-group ">
+                                <label for="exampleInputText">Your Message</label>
+                                <textarea class="form-control" name="message" placeholder="Description of your project...."></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-default">Send Message</button>
+                            <input type="hidden" name="phone2">
+                        </form>
+                        <hr>
+                        <h3>Social media</h3>
+                        <ul class="list-inline banner-social-buttons">
+                            <li><a href="www.twitter.com/ericholdridge"><i class="fa fa-twitter"><span class="network-name">Twitter</span></i></a></li>
+                            <li><a href="www.instagram.com/ericholdridge"><i class="fa fa-instagram"><span class="network-name">Instagram</span></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</body>
+</html>
